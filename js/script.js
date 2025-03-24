a201ba466e97458d9d9433b7fa20adc15dea02ae
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-24 09:51:51 +0100

    [UPD] Email address array

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Esercizio MAIL
+//Creare una lista di indirizzi mail
+const emails = [
+    'prova1@email.it',
+    'prova2@email.it',
+    'prova3@email.it',
+    'prova4@email.it',
+    'prova5@email.it',
+    'prova6@email.it',
+    'prova7@email.it',
+    'prova8@email.it'
+]
+//Chiedere all'utente di inserire al propria mail
+//Verificare che l'utente sia presente nella lista
+//Stampare un messagggio

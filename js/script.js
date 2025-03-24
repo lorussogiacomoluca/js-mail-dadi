0c289fa6d4b8684adaa31f92f189338746e1b9c6
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-24 09:58:03 +0100

    [UPD] Check user email in array

--- js/script.js
+++ js/script.js
@@ -12,5 +12,17 @@ const emails = [
     'prova8@email.it'
 ]
 //Chiedere all'utente di inserire al propria mail
+let user_mail = prompt('Inserisci il tuo indirizzo email: ')
 //Verificare che l'utente sia presente nella lista
+let email_found = false;
+for(let i = 0; i< emails.length; i++){
+    if(user_mail == emails[i]){
+        email_found = true;
+    }
+}
 //Stampare un messagggio
+if (email_found){
+    alert('Indirizzo email trovato!');
+}else{
+    alert('Indirizzo mail non trovato');
+}
